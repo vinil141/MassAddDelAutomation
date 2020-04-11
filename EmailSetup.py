e2033dd15116336dc3ecf058616101407ff96340
vinil141 <vinci141@gmail.com>
2020-04-12 01:02:11 +0530

    Files added for the first time

--- EmailSetup.py
+++ EmailSetup.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python
 # Code to send Email from AMDENV1 in Python v2.6
+"""
+Objective: This code will send email to Amdocs machine from amdenv1
+"""
 import smtplib
 
+sender = 'amdenv1@blp13626001.eezone.bt.com'
+receiver = ['vinil.mehta@amdocs.com']
 
-def email():
 
-    sender = 'amdenv1@blp13626001.eezone.bt.com'
-    receivers = ['vinil.mehta@amdocs.com']
-
+def email(*args):
     message = """From: amdenv1@blp13626001.eezone.bt.com
     To: To Vinil Mehta
     Subject: SMTP e-mail test
 
     This is a test e-mail message.
     """
+    try:
+        smtpObj = smtplib.SMTP('localhost')
+        smtpObj.sendmail(sender, receiver, message)
+        print("Successfully sent email")
+    except smtplib.SMTPException as e:
+        print('Error: unable to send email', e)
 
-   try:
-       smtpObj = smtplib.SMTP('localhost')
-       smtpObj.sendmail(sender,receivers, message)
-	   print "Successfully sent email"
-   except smtplib.SMTPException:
-       print 'Error: unable to send email'
+
+email(sender, receiver)
