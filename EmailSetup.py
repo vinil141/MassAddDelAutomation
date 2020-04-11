168cc8a56fc8c6c8c9e77f1f8e33fa50770af908
vinil141 <62430317+vinil141@users.noreply.github.com>
2020-04-12 01:09:00 +0530

    Hostname & usernam,e
    Hide host name & User name. Replace with actuals

--- EmailSetup.py
+++ EmailSetup.py
@@ -5,13 +5,13 @@ Objective: This code will send email to Amdocs machine from amdenv1
 """
 import smtplib
 
-sender = 'amdenv1@blp13626001.eezone.bt.com'
-receiver = ['vinil.mehta@amdocs.com']
+sender = 'hostname'
+receiver = ['email_id']
 
 
 def email(*args):
-    message = """From: amdenv1@blp13626001.eezone.bt.com
-    To: To Vinil Mehta
+    message = """From: host_name
+    To: To User NAme
     Subject: SMTP e-mail test
 
     This is a test e-mail message.
